7425d9ae9b42bc82fbd9752913fe6a76249067fd
Matthias Tilch <matthiastilch95@gmail.com>
2026-05-22 09:59:24 +0200

    update: add missing conditions for moonrise and moonset in custom_weather_codes

--- app/services/weather_data.py
+++ app/services/weather_data.py
@@ -97,9 +97,9 @@ def custom_weather_codes(old_code, time, sunrise, sunset, moonrise, moonset, tem
         code["special"] = "sunrise"
     elif time - 70 <= sunset <= time + 30:
         code["special"] = "sunset"
-    elif time - 70 <= moonrise <= time + 30:
+    elif time - 70 <= moonrise <= time + 30 and code["time"] == "night-alt":
         code["special"] = "moonrise"
-    elif time -70 <= moonset <= time + 30:
+    elif time -70 <= moonset <= time + 30 and code["time"] == "night-alt":
         code["special"] = "moonset"
     elif time > moonset and time < moonrise and code["time"] == "night-alt":
             code["special"] = "stars"
